87aeb1cb19b31f6615a54cff39221c37fa4bf582
blockmonkey <blockmonkey@naver.com>
2022-12-24 18:31:15 +0900

    Feat : 메뉴조회

--- controller/customer.go
+++ controller/customer.go
@@ -1,9 +1,24 @@
 package controller
 
 import (
+	"net/http"
+	md "pkg/model"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetMenus(ctx *gin.Context) {
-
+	page := ctx.Param("page")
+	page_numb, _ := strconv.Atoi(page)
+	limit := ctx.Param("limit")
+	limit_numb, _ := strconv.Atoi(limit)
+	offset := (page_numb - 1) * limit_numb
+	curResult, err := md.Mmenu.GetMenuPaging(limit_numb, offset)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"payload": curResult,
+	})
 }
--- model/menu_schema.go
+++ model/menu_schema.go
@@ -1,12 +1,15 @@
 package model
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"pkg/config"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Menu struct {
@@ -24,6 +27,17 @@ type Menu struct {
 var Mmenu *Menu
 var Cmenu *mongo.Collection = config.SelectCol(config.DB, "menu")
 
-func (m *Menu) GetMenuPaging(page, limit int) {
-	fmt.Println("GetMenuPaging")
+func (m *Menu) GetMenuPaging(limit, offset int) ([]Menu, error) {
+	var result []Menu
+	limit_int64 := int64(limit)
+	offset_int64 := int64(offset)
+	Opt := options.Find().SetSort(bson.D{{"createdAt", -1}}).SetLimit(limit_int64).SetSkip(offset_int64)
+	cur, err := Cmenu.Find(context.TODO(), bson.M{}, Opt)
+	if err != nil {
+		return nil, errors.New("FIND ERR")
+	}
+	cur.Decode(&result)
+	cur.All(context.TODO(), &result)
+
+	return result, nil
 }
